682ad8628039c75798cb1a2ae57ae67103ef07a9
Daniel <singer.daniel85@gmail.com>
2022-06-29 11:51:19 +0200

    load controllers added

--- controllers/load.controllers.js
+++ controllers/load.controllers.js
@@ -1,11 +1,57 @@
 const asyncHandler = require("express-async-handler");
+const { LOAD } = require("../models/load.model");
 
 /**
  * GET All Loads
  */
 
-const allLoads = asyncHandler(async (req, res) => {});
+const allLoads = asyncHandler(async (req, res) => {
+    const loads = await LOAD.find({});
+});
+
+/**
+ * POST New Load
+ * @description Adds new load to database
+ */
+
+const addLoad = asyncHandler(async(req,res) => {
+    const load = await LOAD.create(req.body);
+    res.status(200).json(load);
+});
+
+/**
+ * GET Specific Load
+ */
+
+const oneLoad = asyncHandler(async(req,res) => {
+    const { _id } = req.params;
+    const load = await LOAD.findById(_id);
+    if(load){
+        res.status(200).json(load);
+    } else {
+        res.status(404);
+        throw new Error('Load konnte nicht gefunden werden')
+    }
+});
+
+/**
+ * DELETE Specific Load
+ */
+
+const deleteLoad = asyncHandler(async(req,res) => {
+    const { _id } = req.params;
+    const load = await LOAD.findByIdAndDelete(_id);
+    if(load){
+        res.status(200).json(load);
+    } else {
+        res.status(400);
+        throw new Error('Load konnte nicht gelöscht werden')
+    }
+});
 
 module.exports = {
   allLoads,
+  addLoad,
+  oneLoad,
+  deleteLoad
 };
--- routes/load.routes.js
+++ routes/load.routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { allLoads } = require("../controllers/load.controllers");
+const { allLoads, oneLoad, addLoad, deleteLoad } = require("../controllers/load.controllers");
 const router = express.Router();
 
 /**
  * @route /api/v1/loads
  */
 
-router.route("/").get(allLoads);
+router.route("/").get(allLoads).post(addLoad);
+
+/**
+ * @route /api/v1/loads/:_id
+ */
+
+router.route('/:_id').get(oneLoad).delete(deleteLoad);
