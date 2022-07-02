f3624e35a5df81b40be5f733250f88555f18ec9e
Daniel <singer.daniel85@gmail.com>
2022-07-02 12:19:49 +0200

    transactions can only be deleted if user is admin

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const SKYDIVER = require('../models/skydiver.model');
+
+/**
+ * @description Middleware can be added to routes to protect them from unauthorized users
+ */
+
+const protect = asyncHandler(async(req,res,next) => {
+
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.skydiver = await SKYDIVER.findById(decoded._id).select('-password');
+            next()
+        } catch (error) {
+            console.error(error);
+            res.status(401);
+            throw new Error('Nicht für Zugriff berechtigt')
+        }
+    };
+
+    if(!token){
+        res.status(401);
+        throw new Error('Nicht Authorisiert. Kein Token zur Verfügung gestellt')
+    };
+});
+
+const admin = (req,res,next) => {
+    if(req.skydiver && req.skydiver.isAdmin){
+        next();
+    } else {
+        res.status(401);
+        throw new Error('Benutzer kein Administrator. Zugriff nicht erlaubt')
+    }
+}
+
+module.exports = {
+    protect,
+    admin
+}
