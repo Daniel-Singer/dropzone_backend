aec08fdfb3619df60d10a03caed27693e3099414
Daniel <singer.daniel85@gmail.com>
2022-07-03 20:35:34 +0200

    added protect middleware to delete load route

--- routes/load.routes.js
+++ routes/load.routes.js
@@ -1,6 +1,12 @@
 const express = require("express");
-const { allLoads, oneLoad, addLoad, deleteLoad } = require("../controllers/load.controllers");
+const {
+  allLoads,
+  oneLoad,
+  addLoad,
+  deleteLoad,
+} = require("../controllers/load.controllers");
 const { loadsByDate } = require("../middleware/load.middleware");
+const { protect } = require("../middleware/auth.middleware");
 const router = express.Router();
 
 /**
@@ -13,6 +19,6 @@ router.route("/").get(loadsByDate, allLoads).post(addLoad);
  * @route /api/v1/loads/:_id
  */
 
-router.route('/:_id').get(oneLoad).delete(deleteLoad);
+router.route("/:_id").get(oneLoad).delete(protect, deleteLoad);
 
 module.exports = router;
